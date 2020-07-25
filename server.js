9a11e46a13f079e9228727f3115ce359ce5ce80c
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-07-25 17:17:21 +0530

    server created

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const cors =  require('cors');
+
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'));
+
+
+
+app.post('/getaudio', (req,res) => {
+	const { link } = req.body;
+	console.log('processing');
+	let stream = ytdl(link, {  filter: 'audioonly' });
+	stream.on('info', function (info, format) {
+	  console.log('Download started')
+	  console.log(info.videoDetails.title)
+	  //console.log(format);
+	})
+	stream.on('progress', function (length, downloaded, total) {
+	  //console.log(`length=${total}`);
+	});
+	stream.on('response', Ytbres => { 
+	  console.log(Ytbres.headers)
+	});
+
+
+	stream.pipe(fs.createWriteStream('public/music.mp3'))
+	res.send('http://localhost:3000/music.mp3')
+})
+
+
+
+app.listen(3000, () => {
+	console.log('listening on 3000');
+})
