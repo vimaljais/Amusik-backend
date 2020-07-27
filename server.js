d9dae40b414cab1ef0f4a514ab029b78f86165e1
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-07-27 22:51:44 +0530

    independent mp3 fetcher

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-
-
 const bodyParser = require('body-parser');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const cors =  require('cors');
 const fetch = require('node-fetch');
-const findRemoveSync = require('find-remove')
-
-
-
-setInterval(() => {
-	const result = findRemoveSync('public', {age: {seconds: 3600}, extensions: ['.mp3', '.log']})
-	console.log(result);
-	}, 360000);
-
-
-
 const express = require('express');
 
 const app = express();
+
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
@@ -28,35 +17,19 @@ const API_KEY_LAST_FM = '7d4f3bdab1f65cad5f3204b2fa02e301';
 
 
 app.get('/', (req, res) => {res.send('it is working!')})
-app.post('/getaudio', (req,res) => {
-	var completed = 0
+
+
+app.post('/link', (req,res) => {
 	const { link } = req.body;
-	console.log(link)
-		console.log('processing');
-		let stream = ytdl(link, {  filter: 'audioonly' });
-		stream.on('info', function (info, format) {
-		  console.log('Download started')
-		  console.log(info.videoDetails.title)
-		  //console.log(format);
-		})
-		stream.on('progress', function (length, downloaded, total) {
-			completed= (downloaded/total)*100
-		  console.log(`completed=${completed}`);
-		  if(downloaded === total)
-		  {
-		  	console.log('give back the response')
-		  	res.json('success')
-		  }
-		});
-		stream.on('response', Ytbres => { 
-		  console.log(Ytbres.headers)
-		});
-
-		stream.pipe(fs.createWriteStream('public/music.mp3'))
-
-		console.log('workingdsadsadsadasdsadsa')	
-	
+	cmd.get(
+ 			`ytdl ${link} --filter audioonly --print-url`
+        ,
+        function(err, data, stderr){
+            res.json(data);
+        }
+    );	
 })
+
 
 app.post('/geturl', (req, res) => {
 	const {name} = req.body;
@@ -96,6 +69,7 @@ app.get('/gettop50', (req,res) => {
 		})
 
 })
+
 
 app.listen(process.env.PORT, () => {
 	console.log(`the port is ${process.env.PORT}`);
