d2f6e3015625416a43ebaa99dcac0b522046feb6
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-07-27 17:47:28 +0530

    top50 support added

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+
+
 const bodyParser = require('body-parser');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const cors =  require('cors');
 const fetch = require('node-fetch');
+const findRemoveSync = require('find-remove')
+
+
+
+setInterval(() => {
+	const result = findRemoveSync('public', {age: {seconds: 3600}, extensions: ['.mp3', '.log']})
+	console.log(result);
+	}, 360000);
+
+
+
+const express = require('express');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +24,7 @@ app.use(cors());
 app.use(express.static('public'));
 
 const API_KEY_GOOGLE = 'AIzaSyDVPd9RdXnQ_0mhFWzGzvAKwAfUEXIfj7s';
+const API_KEY_LAST_FM = '7d4f3bdab1f65cad5f3204b2fa02e301';
 
 
 app.get('/', (req, res) => {res.send('it is working!')})
@@ -58,7 +72,30 @@ app.post('/geturl', (req, res) => {
               })
 })
 
+app.get('/getart/:name', (req,res) => {
+	var {name} = req.params;
 
+	name = name.replace(/ \([\s\S]*?\)/g, '');
+
+	var url = '';
+	var url600= '';
+	fetch(`https://itunes.apple.com/search?term=${name}`)
+	.then(response=> response.json())
+		.then(response => {
+			url = response.results[0].artworkUrl100;
+			url600 = url.replace("100x100bb", "200x200bb");
+			res.json(url600)
+		})
+})
+
+app.get('/gettop50', (req,res) => {
+	fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${API_KEY_LAST_FM}&format=json`)
+	.then(response=> response.json())
+		.then(response => {
+			res.json(response)
+		})
+
+})
 
 app.listen(process.env.PORT, () => {
 	console.log(`the port is ${process.env.PORT}`);
